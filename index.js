e6cd4760b64d01afffd0e3ce6e91c2372fdb5c53
Abhinav Chinnusamy <abhinavchinnusamy@gmail.com>
2024-04-11 11:59:20 +0530

    updated resume page

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 import '@material/web/all.js';
-import '@material/web/icon/icon.js';
+import '@material/web/button/filled-button.js';
+import '@material/web/labs/card/elevated-card.js';
+import '@material/web/labs/card/filled-card.js';
+import '@material/web/labs/card/outlined-card.js';
